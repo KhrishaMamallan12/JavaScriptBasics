7d2e9ddd0220710ccf56e87cba65e5733ab71127
Khrisha Mamallan <125912478+KhrishaMamallan12@users.noreply.github.com>
2023-06-14 22:39:40 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,57 @@
+// 1) Create a variable for each data type (String, Number, Bloat, Boolean).
+/*
+// String
+let myString = "Hello, world!";
+
+// Number
+let myNumber = 42;
+
+// Float 
+let myFloat = 3.14;
+
+// Boolean
+let myBoolean = true;
+
+console.log("String:", myString);
+console.log("Number:", myNumber);
+console.log("Float:", myFloat);
+console.log("Boolean:", myBoolean);
+*/
+
+
+// 2) Use all the above declared variable in a console.log and get the output.
+/*
+console.log("String:", myString);
+console.log("Number:", myNumber);
+console.log("Float:", myFloat);
+console.log("Boolean:", myBoolean);
+*/
+
+// 3) Using VAR, LET and CONST, find the solution for the following values. Alse declare the 
+/*required variables and formular.
+# Area of a circle
+# Circumference of a circle
+# Radius of the circle*/
+/*
+// Constants
+const PI = 3.14159;
+
+// Inputs
+let radius = 5;
+
+// Calculations
+var area = PI * radius * radius;
+let circumference = 2 * PI * radius;
+const diameter = 2 * radius;
+
+// Output
+console.log("Area of the circle:", area);
+console.log("Circumference of the circle:", circumference);
+console.log("Radius of the circle:", radius);
+console.log("Diameter of the circle:", diameter);
+*/
+
+
+
+
+
